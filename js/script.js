5a786fbe64efef3bcecec6372f60c7e8d565d033
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-07 22:43:50 -0600

    actualizacion de funcionalidad en script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,47 @@
 var userMenuDiv = document.getElementById("userMenu");
-    var userMenu = document.getElementById("userButton");
-
-    document.onclick = check;
-
-    function check(e) {
-        var target = (e && e.target) || (event && event.srcElement);
-
-        if (!checkParent(target, userMenuDiv)) {
-
-            if (checkParent(target, userMenu)) {
-
-                if (userMenuDiv.classList.contains("invisible")) {
-                    userMenuDiv.classList.remove("invisible");
-                } else {
-                    userMenuDiv.classList.add("invisible");
-                }
-
+var userMenu = document.getElementById("userButton");
+document.onclick = check;
+function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+    if (!checkParent(target, userMenuDiv)) {
+        if (checkParent(target, userMenu)) {
+            if (userMenuDiv.classList.contains("invisible")) {
+                userMenuDiv.classList.remove("invisible");
             } else {
                 userMenuDiv.classList.add("invisible");
             }
+        } else {
+            userMenuDiv.classList.add("invisible");
         }
     }
-
-    function checkParent(t, elm) {
-        while (t && t.parentNode) {
-
-            if (t == elm) {
-                return true;
-            }
-
-            t = t.parentNode;
+}
+function checkParent(t, elm) {
+    while (t && t.parentNode) {
+        if (t == elm) {
+            return true;
         }
-
-        return false;
+        t = t.parentNode;
     }
+    return false;
+}
 
 var menuButton = document.getElementById("menuButton");
-    var sidebar = document.getElementById("sidebar");
+var sidebar = document.getElementById("sidebar");
+menuButton.addEventListener("click", function () {
+    sidebar.classList.toggle("menu-abierto");
+});
 
-    menuButton.addEventListener("click", function () {
-        sidebar.classList.toggle("menu-abierto");
+let usuarioActivo = sessionStorage.getItem("usuarioActivo");
+
+if (!usuarioActivo) {
+    window.location.href = "login.html";
+} else {
+    document.getElementById("nombreUsuarioNavbar").textContent = usuarioActivo;
+}
+
+let enlacesCerrarSesion = document.querySelectorAll(".enlace-cerrar-sesion");
+enlacesCerrarSesion.forEach(function (enlace) {
+    enlace.addEventListener("click", function () {
+        sessionStorage.removeItem("usuarioActivo");
     });
+});
